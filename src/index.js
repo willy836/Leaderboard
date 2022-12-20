8b6864f483d9ab8fa890c9a7011034bc7edc2c9f
willy836 <wilsonkimiri836@gmail.com>
2022-12-20 20:48:04 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
-import addScore from './modules/addScore';
+import addScore from './modules/addScore.js';
 
 const form = document.querySelector('.form');
 
-
-form.addEventListener('submit', addScore)
+form.addEventListener('submit', addScore);
