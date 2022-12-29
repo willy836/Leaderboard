2f694a3a4cefaa0369b486a92f24de7447d05f91
willy836 <wilsonkimiri836@gmail.com>
2022-12-29 14:48:05 +0300

    Add styling

--- src/index.js
+++ src/index.js
@@ -11,3 +11,20 @@ addScoreToBoard();
 
 const refreshBtn = document.querySelector('.refresh-btn');
 refreshBtn.addEventListener('click', addScoreToBoard);
+
+// const gameId = ()=> {
+//     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             name: "Wilson's game",
+//         })
+//     })
+//     .then(res => res.json())
+//     .then(myId => console.log(myId))
+// }
+// gameId();
+
+// ID = 6doU8vQZxTk2EuYUh4xO
