9a809fd781bd438a2f24f69ff18d9a47953d72f6
willy836 <wilsonkimiri836@gmail.com>
2023-01-21 10:45:52 +0300

    Fix linter errors

--- src/modules/postScore.js
+++ src/modules/postScore.js
@@ -1,11 +1,10 @@
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6doU8vQZxTk2EuYUh4xO/scores/";
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6doU8vQZxTk2EuYUh4xO/scores/';
 
 const postScore = async (user, score) => {
   const response = await fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "content-type": "application/json; charset=UTF-8",
+      'content-type': 'application/json; charset=UTF-8',
     },
     body: JSON.stringify({
       user: `${user}`,
